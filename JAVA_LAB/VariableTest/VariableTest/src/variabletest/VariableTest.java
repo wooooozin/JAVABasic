124b584857edb76c26a1e9b347376d1cf9a5dd83
wooooozin <wooooozin@gmail.com>
2023-03-05 08:40:33 +0900

    VariableTest

---
+++ JAVA_LAB/VariableTest/VariableTest/src/variabletest/VariableTest.java
@@ -0,0 +1,17 @@
+package variabletest;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		int age;
+		age = 10;
+		int level = 1000;
+		// int numberOfStudent; (O)
+		// int ns; (X)
+		
+		System.out.println(age);
+		System.out.println(level);
+	}
+
+}
