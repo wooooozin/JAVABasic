32c0479863e4e78225ffb88f7716e964b7c52fc0
wooooozin <wooooozin@gmail.com>
2023-03-05 16:55:37 +0900

    데이터 타입 - 문자형

--- JAVA_LAB/DataTypeChar/DataTypeChar/src/charater/CharacterTest.java
+++ JAVA_LAB/DataTypeChar/DataTypeChar/src/charater/CharacterTest.java
@@ -3,7 +3,21 @@ package charater;
 public class CharacterTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		char ch1 = 'A';
+		System.out.println(ch1); // A
+		System.out.println((int)ch1); // 65
+		
+		char ch2 = 66;
+		System.out.println(ch2); // B
+		
+		char ch3 = '한';
+		char ch4 = '\uD55C';
+		char ch5 = '\uAC00';
+		System.out.println(ch3); // 한
+		System.out.println(ch4); // 한
+		System.out.println(ch5); // 
+		
 
 	}
 
