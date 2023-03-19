2de707aff5513d700c1533a6eb0fee51ccd07663
wooooozin <wooooozin@gmail.com>
2023-03-20 06:54:04 +0900

    메서드 재정의와 가상 메서드의 원리

--- JAVA_LAB/OPP_Core/OopCore/src/methodCall/TestMehod.java
+++ JAVA_LAB/OPP_Core/OopCore/src/methodCall/TestMehod.java
@@ -2,4 +2,19 @@ package methodCall;
 
 public class TestMehod {
 
+	int num;
+	
+	void aaa() {
+		System.out.println("aaa() 호출");
+	}
+	
+	public static void main(String[] args) {
+		
+		TestMehod a1 = new TestMehod();
+		a1.aaa();
+		
+		TestMehod a2 = new TestMehod();
+		a2.aaa();
+	}
+
 }
