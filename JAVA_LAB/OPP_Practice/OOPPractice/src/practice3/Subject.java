07395294b6976a796e8f1dd0ea66dca51268a321
wooooozin <wooooozin@gmail.com>
2023-03-19 15:13:40 +0900

    객체지향 기초 문제풀이

---
+++ JAVA_LAB/OPP_Practice/OOPPractice/src/practice3/Subject.java
@@ -0,0 +1,23 @@
+package practice3;
+
+public class Subject {
+	
+	private String name;
+	private int scorePoint;
+	
+	
+	public int getScorePoint() {
+		return scorePoint;
+	}
+	public void setScorePoint(int scorePoint) {
+		this.scorePoint = scorePoint;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
